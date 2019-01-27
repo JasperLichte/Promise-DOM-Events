b0bfc928ec52b992966becf5b0c2d5a9cc827199
JasperLichte <contact@jasper-lichte.com>
2019-01-27 10:53:46 +0100

    Fix config

--- index.js
+++ index.js
@@ -7,8 +7,5 @@ export default
           && reject(new Error('No valid DOM Element provided!'))
         !eventType && reject(new Error('No event type provided!'))
 
-        domElement.addEventListener(eventType, e => {
-          resolve(e)
-          domElement.removeEventListener(eventType, resolve, true)
-        }, true)
+        domElement.addEventListener(eventType, resolve, {once: true})
       })
